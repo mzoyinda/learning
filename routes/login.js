219043999559a81fd292af600bcddef2791cad8a
AbdussamadYisau <sammieyisau@gmail.com>
2021-01-18 15:26:08 +0100

    Added necessary validation for sign up route

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const databaseConn = require("../models/databaseConn");
 const router = express.Router();
 
 
@@ -9,10 +10,43 @@ router.get('/login', (req,res,next) => {
 
 
 router.post('/login', (req,res,next) => {
-    let name = req.body.name;
-    let password = req.body.psw;
+    const email = req.body.email;
+    const password = req.body.psw;
 
-    res.setHeader('Set-Cookie', 'loggedIn=true');
-    res.redirect('/');
+    if(email && password) {
+    
+        databaseConn.dbVar.query('SELECT * FROM accounts WHERE email = ?', [email], (_err, results, fields) => {
+        
+            let validPass =  bcrypt.compareSync(password, results[0].hash);
+
+            //  check password entered against hashed password in db
+
+            if (validPass) {
+                if(results.length > 0) {
+                    req.session.loggedin = true;
+                    req.session.username = email;
+        
+
+                    
+                }
+            }
+                else {
+                    res.status(404).json({
+                        message: 'Incorrect Username and/or Password!'
+                    });
+                }
+
+            
+        
+        
+            });
+    } else {
+        res.json('Please enter Username and Password');
+        res.json({
+            message: 'Please enter Email and Password'
+        });
+    
+    }
+    
 });
 module.exports = router;
--- routes/signUp.js
+++ routes/signUp.js
@@ -1,32 +1,57 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
-const { body, validationResult } = require("express-validator");
+const validator = require("email-validator");
+const { body, check, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Database Connection Route
 const databaseConn = require("../models/databaseConn");
 
 
-// [
-//     body("email").isEmail().withMessage("Please enter a valid Email Address"),
-//     body('password').trim().isLength({min:5}),
-//     body('name').trim().not().isEmpty()
-// ]
 
-router.put("/signUp", async (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//       const error = new Error('Validation failed');
-//       error.statusCode = 422;
-//       error.data = errors.array();
-//       throw error;
-//   }
-    const name = req.body.name;
+
+router.put("/signUp",
+        [
+            // body('email').isEmail().normalizeEmail().withMessage("Please enter a valid Email Address"),
+            // body('psw').trim().isLength({min:5}),
+            // body('name').trim().not().isEmpty()
+            check('name')
+                .exists()
+                .trim()
+                .matches(/^[a-zA-Z\ö\ç\ş\ı\ğ\ü\Ö\Ç\Ş\İ\Ğ\Ü ]{3,16}$/)
+                .custom(async username => {
+                    const value = await isNameInUse(username);
+                    if (value) {
+                        throw new Error('Name already exists!');
+                    }
+                })
+                .withMessage('Invalid username!'),
+            check('email')
+                .exists()
+                .isLength({ min: 6, max: 100 })
+                .isEmail()
+                .normalizeEmail()
+                .trim()
+                .custom(async email => {
+                    const value = await isEmailInUse(email);
+                    if (value) {
+                        throw new Error('Email already exists!!!');
+                    }
+                })
+                .withMessage('Invalid email address!'),
+        ],    
+async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+
+    return res.status(400).json({ errors: errors.array() });
+  }
+    const username = req.body.name;
     const password = req.body.psw;
     const email = req.body.email;
     const hash = await bcrypt.hashSync(password, 10);
 
-    let account = {username: name, 
+    let account = {username: username, 
                    hash: hash, 
                    email: email
                    };
@@ -35,15 +60,43 @@ router.put("/signUp", async (req, res, next) => {
     let query = databaseConn.dbVar.query(sql, account, (err,result) => {
     
         if(err) console.log(err.code);
-        console.log(result);        
+              
     });
 
     res.status(201).json({
         message: 'User created'
     });
 
-
-
  
 });
+
+
+function isEmailInUse(email){
+    return new Promise((resolve, reject) => {
+        databaseConn.dbVar.query('SELECT COUNT(*) AS total FROM accounts WHERE email = ?', [email], function (error, results, fields) {
+            if(!error){
+                console.log("EMAIL COUNT : "+results[0].total);
+                return resolve(results[0].total > 0);
+            } else {
+                return reject(new Error('Database error!!'));
+            }
+          }
+        );
+    });
+}
+
+function isNameInUse(username){
+    return new Promise((resolve, reject) => {
+        databaseConn.dbVar.query('SELECT COUNT(*) AS total FROM accounts WHERE username = ?', [username], function (error, results, fields) {
+            if(!error){
+                console.log("Name : "+results[0].total);
+                return resolve(results[0].total > 0);
+            } else {
+                return reject(new Error('Database error!!'));
+            }
+          }
+        );
+    });
+}
+
 module.exports = router;
