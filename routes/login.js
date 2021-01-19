2ec6e6e15d82a77c44145f688f66ca4dfbea3d5a
AbdussamadYisau <sammieyisau@gmail.com>
2021-01-19 09:53:13 +0100

    Implemented login route and authentication

--- routes/login.js
+++ routes/login.js
@@ -1,52 +1,55 @@
-const express = require('express');
+const express = require("express");
 const databaseConn = require("../models/databaseConn");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
+// router.get("/login", (req, res, next) => {
+//   console.log(req.get("Cookie"));
+// });
 
+router.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.psw;
 
-router.get('/login', (req,res,next) => {
-    console.log(req.get('Cookie'));
-});
+  if (email && password) {
+    databaseConn.dbVar.query(
+      "SELECT * FROM accounts WHERE email = ?",
+      [email],
+      (_err, results, fields) => {
+        let validPass = bcrypt.compareSync(password, results[0].hash);
 
+        //  check password entered against hashed password in db
 
-router.post('/login', (req,res,next) => {
-    const email = req.body.email;
-    const password = req.body.psw;
+        if (validPass) {
+          if (results.length > 0) {
+            req.session.loggedin = true;
+            req.session.username = email;
+            const token = jwt.sign(
+              {
+                email,
+                userId: results[0].id.toString()
+              },
+              "someSuperSecret",
+              { expiresIn: "1hr" }
+            );
 
-    if(email && password) {
-    
-        databaseConn.dbVar.query('SELECT * FROM accounts WHERE email = ?', [email], (_err, results, fields) => {
-        
-            let validPass =  bcrypt.compareSync(password, results[0].hash);
-
-            //  check password entered against hashed password in db
-
-            if (validPass) {
-                if(results.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.username = email;
-        
-
-                    
-                }
-            }
-                else {
-                    res.status(404).json({
-                        message: 'Incorrect Username and/or Password!'
-                    });
-                }
-
-            
-        
-        
+            res.status(200).json({
+              token: token,
+              userId: results[0].id.toString()
             });
-    } else {
-        res.json('Please enter Username and Password');
-        res.json({
-            message: 'Please enter Email and Password'
-        });
-    
-    }
-    
+          }
+        } else {
+          res.status(404).json({
+            message: "Incorrect Email and/or Password!",
+          });
+        }
+      }
+    );
+  } else {
+    res.json({
+      message: "Please enter Email and Password",
+    });
+  }
 });
 module.exports = router;
--- routes/signUp.js
+++ routes/signUp.js
@@ -1,102 +1,94 @@
 const express = require("express");
-const bcrypt = require('bcryptjs');
-const validator = require("email-validator");
+const bcrypt = require("bcryptjs");
 const { body, check, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Database Connection Route
 const databaseConn = require("../models/databaseConn");
 
-
-
-
-router.put("/signUp",
-        [
-            // body('email').isEmail().normalizeEmail().withMessage("Please enter a valid Email Address"),
-            // body('psw').trim().isLength({min:5}),
-            // body('name').trim().not().isEmpty()
-            check('name')
-                .exists()
-                .trim()
-                .matches(/^[a-zA-Z\ö\ç\ş\ı\ğ\ü\Ö\Ç\Ş\İ\Ğ\Ü ]{3,16}$/)
-                .custom(async username => {
-                    const value = await isNameInUse(username);
-                    if (value) {
-                        throw new Error('Name already exists!');
-                    }
-                })
-                .withMessage('Invalid username!'),
-            check('email')
-                .exists()
-                .isLength({ min: 6, max: 100 })
-                .isEmail()
-                .normalizeEmail()
-                .trim()
-                .custom(async email => {
-                    const value = await isEmailInUse(email);
-                    if (value) {
-                        throw new Error('Email already exists!!!');
-                    }
-                })
-                .withMessage('Invalid email address!'),
-        ],    
-async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-
-    return res.status(400).json({ errors: errors.array() });
-  }
+router.put(
+  "/signUp",
+  [
+    check("name")
+      .exists()
+      .trim()
+      .matches(/^[a-zA-Z\ö\ç\ş\ı\ğ\ü\Ö\Ç\Ş\İ\Ğ\Ü ]{3,16}$/)
+      .custom(async (username) => {
+        const value = await isNameInUse(username);
+        if (value) {
+          throw new Error("Name already exists!");
+        }
+      })
+      .withMessage("Invalid username!"),
+    check("email")
+      .exists()
+      .isLength({ min: 6, max: 100 })
+      .isEmail()
+      .normalizeEmail()
+      .trim()
+      .custom(async (email) => {
+        const value = await isEmailInUse(email);
+        if (value) {
+          throw new Error("Email already exists!!!");
+        }
+      })
+      .withMessage("Invalid email address!"),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const username = req.body.name;
     const password = req.body.psw;
     const email = req.body.email;
     const hash = await bcrypt.hashSync(password, 10);
 
-    let account = {username: username, 
-                   hash: hash, 
-                   email: email
-                   };
-    let sql = 'INSERT INTO accounts SET ? ';
+    let account = { username: username, hash: hash, email: email };
+    let sql = "INSERT INTO accounts SET ? ";
 
-    let query = databaseConn.dbVar.query(sql, account, (err,result) => {
-    
-        if(err) console.log(err.code);
-              
+    let query = databaseConn.dbVar.query(sql, account, (err, result) => {
+      if (err) console.log(err.code);
     });
 
     res.status(201).json({
-        message: 'User created'
+      message: "User created",
     });
+  }
+);
 
- 
-});
-
-
-function isEmailInUse(email){
-    return new Promise((resolve, reject) => {
-        databaseConn.dbVar.query('SELECT COUNT(*) AS total FROM accounts WHERE email = ?', [email], function (error, results, fields) {
-            if(!error){
-                console.log("EMAIL COUNT : "+results[0].total);
-                return resolve(results[0].total > 0);
-            } else {
-                return reject(new Error('Database error!!'));
-            }
-          }
-        );
-    });
+function isEmailInUse(email) {
+  return new Promise((resolve, reject) => {
+    databaseConn.dbVar.query(
+      "SELECT COUNT(*) AS total FROM accounts WHERE email = ?",
+      [email],
+      function (error, results, fields) {
+        if (!error) {
+          console.log("EMAIL COUNT : " + results[0].total);
+          return resolve(results[0].total > 0);
+        } else {
+          return reject(new Error("Database error!!"));
+        }
+      }
+    );
+  });
 }
 
-function isNameInUse(username){
-    return new Promise((resolve, reject) => {
-        databaseConn.dbVar.query('SELECT COUNT(*) AS total FROM accounts WHERE username = ?', [username], function (error, results, fields) {
-            if(!error){
-                console.log("Name : "+results[0].total);
-                return resolve(results[0].total > 0);
-            } else {
-                return reject(new Error('Database error!!'));
-            }
-          }
-        );
-    });
+function isNameInUse(username) {
+  return new Promise((resolve, reject) => {
+    databaseConn.dbVar.query(
+      "SELECT COUNT(*) AS total FROM accounts WHERE username = ?",
+      [username],
+      function (error, results, fields) {
+        if (!error) {
+          console.log("Name : " + results[0].total);
+          return resolve(results[0].total > 0);
+        } else {
+          return reject(new Error("Database error!!"));
+        }
+      }
+    );
+  });
 }
 
 module.exports = router;
