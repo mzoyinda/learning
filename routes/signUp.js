cfdd60e15577a8d294e64f207e3d4d8a8778b4fd
AbdussamadYisau <sammieyisau@gmail.com>
2021-02-05 02:11:04 +0100

    fix: error code upon signup

--- routes/signUp.js
+++ routes/signUp.js
@@ -37,7 +37,7 @@ router.put(
   async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).send({ errors: errors.array() });
     }
     const username = req.body.name;
     const password = req.body.psw;
